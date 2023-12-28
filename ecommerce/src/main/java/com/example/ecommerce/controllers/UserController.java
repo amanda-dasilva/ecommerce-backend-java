82497598d9022c29cc8609aad90da24a6e16aacf
amanda-dasilva <dinsil221@gmail.com>
2023-12-27 23:20:17 -0300

    created a UserController class in the controllers directory

---
+++ ecommerce/src/main/java/com/example/ecommerce/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.controllers;
+
+import com.example.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("user")
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+}
