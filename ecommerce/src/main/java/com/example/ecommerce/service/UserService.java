b4947af411a374f591cef04fcd2ef488e33ecb88
amanda-dasilva <dinsil221@gmail.com>
2023-12-27 23:18:22 -0300

    created a UserService class in the Service package

---
+++ ecommerce/src/main/java/com/example/ecommerce/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+}
