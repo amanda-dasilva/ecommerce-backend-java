a547c8d9bb2a4e910404b2faab9a41ccb9fb5e0c
amanda-dasilva <dinsil221@gmail.com>
2024-01-02 14:43:26 -0300

    created a repository for the whishlist model

---
+++ ecommerce/src/main/java/com/example/ecommerce/repository/WishListRepository.java
@@ -0,0 +1,17 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.model.User;
+import com.example.ecommerce.model.WishList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WishListRepository extends JpaRepository<WishList, Integer> {
+
+    // find all the products saved to wishlist for a user
+    // sort by latest created date
+    List<WishList> findAllByUserOrderByCreatedDateDesc(User user);
+
+}
