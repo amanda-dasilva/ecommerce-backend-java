6c2411ea38d52b64cffa1131b302f92d39111158
amanda-dasilva <dinsil221@gmail.com>
2024-01-02 17:06:26 -0300

    made adjustments to the whishlist service

--- ecommerce/src/main/java/com/example/ecommerce/service/WishListService.java
+++ ecommerce/src/main/java/com/example/ecommerce/service/WishListService.java
@@ -2,7 +2,6 @@ package com.example.ecommerce.service;
 
 import com.example.ecommerce.model.User;
 import com.example.ecommerce.model.WishList;
-import com.example.ecommerce.repository.UserRepository;
 import com.example.ecommerce.repository.WishListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
