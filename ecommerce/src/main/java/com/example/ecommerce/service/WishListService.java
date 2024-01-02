29cb1a998bd3f9915d006385da0957ad2571732f
amanda-dasilva <dinsil221@gmail.com>
2024-01-02 14:47:06 -0300

    created a service for the whishlist model

---
+++ ecommerce/src/main/java/com/example/ecommerce/service/WishListService.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.model.User;
+import com.example.ecommerce.model.WishList;
+import com.example.ecommerce.repository.UserRepository;
+import com.example.ecommerce.repository.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WishListService {
+
+    private final WishListRepository wishListRepository;
+
+    @Autowired
+    public WishListService(WishListRepository wishListRepository) {
+        this.wishListRepository = wishListRepository;
+    }
+
+    public void createWishlist(WishList wishList) {
+        wishListRepository.save(wishList);
+    }
+
+    public List<WishList> readWishList(User user) {
+        return wishListRepository.findAllByUserOrderByCreatedDateDesc(user);
+    }
+}
