bb3f0cf2deee7d82b1cb6b797d9b8b4415a92f19
amanda-dasilva <dinsil221@gmail.com>
2024-01-04 19:26:20 -0300

    created a cart item dto

---
+++ ecommerce/src/main/java/com/example/ecommerce/dto/cart/CartItemDto.java
@@ -0,0 +1,41 @@
+package com.example.ecommerce.dto.cart;
+
+import com.example.ecommerce.model.Cart;
+import com.example.ecommerce.model.Product;
+import jakarta.validation.constraints.NotNull;
+
+public class CartItemDto {
+    private Integer id;
+    private @NotNull Integer quantity;
+    private @NotNull Product product;
+
+    public CartItemDto(Cart cart) {
+        this.setId(cart.getId());
+        this.setQuantity(cart.getQuantity());
+        this.setProduct(cart.getProduct());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
