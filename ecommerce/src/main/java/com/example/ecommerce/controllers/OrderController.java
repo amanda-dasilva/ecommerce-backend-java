6390e512dd31ba26df906051537aaea584677cc6
amanda-dasilva <dinsil221@gmail.com>
2024-01-16 15:18:01 -0300

    updated the OrderController

--- ecommerce/src/main/java/com/example/ecommerce/controllers/OrderController.java
+++ ecommerce/src/main/java/com/example/ecommerce/controllers/OrderController.java
@@ -1,18 +1,18 @@
 package com.example.ecommerce.controllers;
 
+import com.example.ecommerce.common.ApiResponse;
 import com.example.ecommerce.dto.checkout.CheckoutItemDto;
 import com.example.ecommerce.dto.checkout.StripeResponse;
+import com.example.ecommerce.exceptions.AuthenticationFailException;
+import com.example.ecommerce.model.User;
+import com.example.ecommerce.service.AuthenticationService;
 import com.example.ecommerce.service.OrderService;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,10 +21,12 @@ import java.util.List;
 public class OrderController {
 
     private final OrderService orderService;
+    private final AuthenticationService authenticationService;
 
     @Autowired
-    public OrderController(OrderService orderService){
+    public OrderController(OrderService orderService, AuthenticationService authenticationService){
         this.orderService = orderService;
+        this.authenticationService = authenticationService;
     }
     // stripe create session API
     @PostMapping("/create-checkout-session")
@@ -34,5 +36,16 @@ public class OrderController {
         StripeResponse stripeResponse = new StripeResponse(session.getId());
         // send the stripe session id in response
         return new ResponseEntity<>(stripeResponse, HttpStatus.OK);
+    }
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> placeOrder(@RequestParam("token") String token, @RequestParam("sessionId") String sessionId)
+            throws AuthenticationFailException {
+        // validate token
+        authenticationService.authenticate(token);
+        // retrieve user
+        User user = authenticationService.getUser(token);
+        // place the order
+        orderService.placeOrder(user, sessionId);
+        return new ResponseEntity<>(new ApiResponse(true, "Order has been placed"), HttpStatus.CREATED);
     }
 }
